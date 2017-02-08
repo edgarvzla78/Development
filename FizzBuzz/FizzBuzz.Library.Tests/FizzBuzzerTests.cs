6bf3127ea7c87d5eabd03eeed6a5850dc2dc6a7c
edgarvzla78 <edgarvzla@outlook.com>
2017-02-08 09:36:27 -0800

    FizzBuzz
    FizzBuzz VS2015 Solution

---
+++ FizzBuzz/FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Library.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [Test]
+        public void Buzzer_WhenDefault_ReturnsInput([Values(1,2,4,7,8,11,13,14,16,17,19)] int input)
+        {
+            //Act
+            string output = FizzBuzzer.GetValue(input);
+            //Assert
+            Assert.AreEqual(input.ToString(), output);
+        }
+
+     
+        [Test]
+        public void Buzzer_WhenDiv3_ReturnsFizz([Values(3,6,9,12,18)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [Test]
+        public void Buzzer_When5_ReturnsBuzz([Values(5,10,20)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Buzz", output);
+        }
+
+        [Test]
+        public void Buzzer_WhenDiv3AndDiv5_ReturnsFizzBuzz([Values(15)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("FizzBuzz", output);
+        }
+    }
+}
